c89a8cc4e593c172ba4573227bf8e5409a9ebc77
Malikfazlehadi-555 <malikfazlehadi03333@gmail.com>
2024-07-31 18:09:47 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(Document).ready(function(){
+$('.slider').slick({
+    arrows:false
+    dots:true
+});
+
+
+
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+
+hamberger.addEventListener('click', function(){
+
+});
+
+times.addEventListener('click', function(){
+
+});
+
+});
